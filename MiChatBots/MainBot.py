a3a2127904639b9e67df95447f8ffcfa6699898d
Juan Camilo Blandon Lopez <jblandol77@gmail.com>
2023-04-06 18:27:57 -0500

    "Se crea red neuronal"

--- MiChatBots/MainBot.py
+++ MiChatBots/MainBot.py
@@ -27,7 +27,7 @@ for contenido in datos["contenido"]:
         if contenido ["tag"] not in tags:
             tags.append(contenido["tag"])
 
-palabras = [stemmer.stem(w.lower()) for w in palabras if w!="?"]
+palabras = [stemmer.stem(w.lower()) for w in palabras if w!="*"]
 palabras = sorted(list(set(palabras)))
 tags = sorted(tags)
 
@@ -48,5 +48,35 @@ for x, documento in enumerate(auxX):
     entrenamiento.append(cubeta)
     salida.append(filaSalida)
 
-print(entrenamiento)
-print(salida)
+entrenamiento=numpy.array(entrenamiento)
+salida=numpy.array(salida)
+tensorflow.compat.v1.reset_default_graph()
+red=tflearn.input_data(shape=[None,len(entrenamiento[0])])
+red=tflearn.fully_connected(red,10)
+red=tflearn.fully_connected(red,10)
+red=tflearn.fully_connected(red,len(salida[0]), activation="softmax")
+red=tflearn.regression(red)
+
+modelo=tflearn.DNN(red)
+modelo.fit(entrenamiento, salida, n_epoch=1000,batch_size=10, show_metric=True)
+modelo.save("modelo.tflearn")
+
+def mainBot():
+    while True:
+        entrada = input("Tu: ")
+        cubeta = [0 for _ in range(len(palabras))]
+        entradaProcesada = nltk.word_tokenize(entrada)
+        entradaProcesada = [stemmer.stem(palabra.lower()) for palabra in entradaProcesada]
+        for palabraIndividual in entradaProcesada:
+            for i,palabra in enumerate(palabras):
+                if palabra == palabraIndividual:
+                    cubeta[i] = 1
+        resultados = modelo.predict([numpy.array(cubeta)])
+        resultadosIndices = numpy.argmax(resultados)
+        tag = tags[resultadosIndices]
+
+        for tagAux in datos["contenido"]:
+            if tagAux["tag"] == tag:
+                respuesta = tagAux["respuestas"]
+        print("BOT: ", random.choice(respuesta))
+mainBot()

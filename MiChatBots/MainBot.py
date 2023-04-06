ecf26e466e7e6b2d84949f3f5cfe8b0f14bfc191
Juan Camilo Blandon Lopez <jblandol77@gmail.com>
2023-04-06 18:57:03 -0500

    "Se controla la carga de datos ya analizados"

--- MiChatBots/MainBot.py
+++ MiChatBots/MainBot.py
@@ -9,47 +9,54 @@ import random
 import pickle 
 
 nltk.download('punkt')
-with open('contenido.json') as archivo:
+with open('contenido.json', encoding='utf-8') as archivo:
     datos = json.load(archivo)
 
-palabras=[]
-tags=[]
-auxX=[]
-auxY=[]
+try:
+    with open("variable.pickle", "rb") as archivoPickle:
+        palabras, tags, entrenamiento, salida = pickle.load(archivoPickle)
+except:
+    palabras=[]
+    tags=[]
+    auxX=[]
+    auxY=[]
 
-for contenido in datos["contenido"]:
-    for patrones in contenido["patrones"]:
-        auxPalabra = nltk.word_tokenize(patrones)
-        palabras.extend(auxPalabra)
-        auxX.append(auxPalabra)
-        auxY.append(contenido["tag"])
+    for contenido in datos["contenido"]:
+        for patrones in contenido["patrones"]:
+            auxPalabra = nltk.word_tokenize(patrones)
+            palabras.extend(auxPalabra)
+            auxX.append(auxPalabra)
+            auxY.append(contenido["tag"])
 
-        if contenido ["tag"] not in tags:
-            tags.append(contenido["tag"])
+            if contenido ["tag"] not in tags:
+                tags.append(contenido["tag"])
 
-palabras = [stemmer.stem(w.lower()) for w in palabras if w!="*"]
-palabras = sorted(list(set(palabras)))
-tags = sorted(tags)
+    palabras = [stemmer.stem(w.lower()) for w in palabras if w!="*"]
+    palabras = sorted(list(set(palabras)))
+    tags = sorted(tags)
 
-entrenamiento=[]
-salida=[]
-salidaVacia=[0 for _ in range(len(tags))]
+    entrenamiento=[]
+    salida=[]
+    salidaVacia=[0 for _ in range(len(tags))]
 
-for x, documento in enumerate(auxX):
-    cubeta=[]
-    auxPalabra=[stemmer.stem(w.lower())for w in documento]
-    for w in palabras:
-        if w in auxPalabra:
-            cubeta.append(1)
-        else:
-            cubeta.append(0)
-    filaSalida=salidaVacia[:]
-    filaSalida[tags.index(auxY[x])]=1
-    entrenamiento.append(cubeta)
-    salida.append(filaSalida)
+    for x, documento in enumerate(auxX):
+        cubeta=[]
+        auxPalabra=[stemmer.stem(w.lower())for w in documento]
+        for w in palabras:
+            if w in auxPalabra:
+                cubeta.append(1)
+            else:
+                cubeta.append(0)
+        filaSalida=salidaVacia[:]
+        filaSalida[tags.index(auxY[x])]=1
+        entrenamiento.append(cubeta)
+        salida.append(filaSalida)
+        entrenamiento=numpy.array(entrenamiento)
+        salida=numpy.array(salida)
+        with open("variable.pickle", "wb") as archivoPickle:
+            pickle.dump((palabras, tags, entrenamiento, salida),archivoPickle)
 
-entrenamiento=numpy.array(entrenamiento)
-salida=numpy.array(salida)
+
 tensorflow.compat.v1.reset_default_graph()
 red=tflearn.input_data(shape=[None,len(entrenamiento[0])])
 red=tflearn.fully_connected(red,10)
@@ -58,8 +65,11 @@ red=tflearn.fully_connected(red,len(salida[0]), activation="softmax")
 red=tflearn.regression(red)
 
 modelo=tflearn.DNN(red)
-modelo.fit(entrenamiento, salida, n_epoch=1000,batch_size=10, show_metric=True)
-modelo.save("modelo.tflearn")
+try:
+    modelo.load("modelo.tflearn")
+except:
+    modelo.fit(entrenamiento, salida, n_epoch=1000,batch_size=10, show_metric=True)
+    modelo.save("modelo.tflearn")
 
 def mainBot():
     while True:
